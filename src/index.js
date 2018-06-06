befda4cca688e0caa35f5f9fa66cd556f49beb73
David Gibson <dgibson314@gmail.com>
2018-06-06 00:48:58 -0400

    Adding board

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function Square(props) {
+    return (
+        <button className="square">
+            {props.value}
+        </button>
+        );
+}
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return (
+            <Square
+                value={this.props.squares[i]}
+}
+
 class App extends React.Component {
     render() {
         return <h1>Hello!</h1>
